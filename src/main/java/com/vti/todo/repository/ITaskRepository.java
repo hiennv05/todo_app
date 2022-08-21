2d83b39676d3b3ebec69ffe93a9523685b403dda
hiennguyen <bg.hien@gmail.com>
2022-08-21 10:35:33 +0900

    update 1

--- src/main/java/com/vti/todo/repository/ITaskRepository.java
+++ src/main/java/com/vti/todo/repository/ITaskRepository.java
@@ -1,13 +1,8 @@
 package com.vti.todo.repository;
 
-import com.vti.todo.dto.request.TaskRequest;
-import com.vti.todo.entity.TaskStatus;
 import com.vti.todo.entity.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
--- src/main/java/com/vti/todo/repository/IWorkSpaceRepository.java
+++ src/main/java/com/vti/todo/repository/IWorkSpaceRepository.java
@@ -13,17 +13,7 @@ public interface IWorkSpaceRepository extends JpaRepository<WorkSpace, Integer> 
     @Query("SELECT (count(w) = 0) FROM WorkSpace w WHERE name = ?1")
     boolean isWorkSpaceNotExists(String name);
 
-//    @Query(value = "SELECT w FROM WorkSpace w WHERE w.account.id = ?1")
-//    List<WorkSpace> findWorkSpaceByAccId(Integer id);
-
-//    @Query(value = "SELECT w FROM WorkSpace w WHERE w.account.email= ?1")
-//    List<WorkSpaceResponse> findWorkSpaceByAccEmail(String email);
-
     @Query(value = "SELECT NEW com.vti.todo.dto.response.WorkSpaceResponse(w.id, w.name, w.numberOfTask) FROM WorkSpace w WHERE w.account.email= ?1")
     List<WorkSpaceResponse> findWorkSpaceByAccEmail(String email);
-//
-//    SELECT NEW com.vti.todo.dto.response.WorkSpaceResponse(w.id, w.name, count(t))
-//    FROM WorkSpaceEntity w left join TaskEntity t ON w = t.workSpace
-//    where w.account.email = :email
-//    GROUP BY w
+
 }
